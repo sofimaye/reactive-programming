248e10d39ee305308cca364ee295848271448fad
Sofi.Maye <sofi.mann99@gmail.com>
2023-11-10 17:26:25 +0200

    initial version

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+//вказує на те як запускати програму
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
