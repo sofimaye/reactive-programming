844b9143419259a9764a70116174e9eb76c4d078
Sofi.Maye <sofi.mann99@gmail.com>
2023-11-11 21:14:48 +0200

    lab1 part2 4task event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,23 +3,38 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
-    <p>Ім'я: {{ name }}</p>
-    <p>Вік: {{ age }}</p>
-    <input type="text" [value]="name" /><br/>
-    <input type="text" [value]="age" />
-    <p>Робота: {{ jobTitle }}</p>
-    <p>Місто: {{ city }}</p>
+    <p>Ім'я: {{name}}</p>
+    <p>Вік: {{age}}</p>
+    <input type="text" [value]="name" (input)="onNameChange($event)" /><br/>
+    <input type="text" [value]="age" (input)="onAgeChange($event)" />
+    <p [textContent]="name"></p>
     <table border="1">
       <tr><td [attr.colspan]="colspan">One-Two</td></tr>
       <tr><td>Three</td><td>Four</td></tr>
       <tr><td>Five</td><td>Six</td></tr>
     </table>
+    <p>Кількість кліків: {{count}}</p>
+    <button (click)="increase()">Click</button>
   `
 })
 export class AppComponent {
     name = 'Tom';
     age = 25;
-    jobTitle = 'Інженер';
-    city = 'Київ';
     colspan = 2;
+    count = 0;
+
+    increase(): void {
+        this.count++;
+    }
+
+    onNameChange(event: Event): void {
+        const input = event.target as HTMLInputElement;
+        this.name = input.value;
+    }
+
+    onAgeChange(event: Event): void {
+        const input = event.target as HTMLInputElement;
+        this.age = +input.value; // The '+' operator converts the string to a number
+    }
+
 }
