d9a42e3c296dc8d16c82c5c5b9123d4b30c1e351
Sofi.Maye <sofi.mann99@gmail.com>
2023-11-11 21:32:56 +0200

    lab1 part2 5task two-way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,17 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
-    <p>Ім'я: {{name}}</p>
-    <p>Вік: {{age}}</p>
-    <input type="text" [value]="name" (input)="onNameChange($event)" /><br/>
-    <input type="text" [value]="age" (input)="onAgeChange($event)" />
-    <p [textContent]="name"></p>
+    <p>Ім'я: {{ name }}</p>
+    <p>Вік: {{ age }}</p>
+    <input type="text" [(ngModel)]="name" /><br/>
+    <input type="number" [(ngModel)]="age" />
+    <p>Текст: {{ name }}</p>
     <table border="1">
       <tr><td [attr.colspan]="colspan">One-Two</td></tr>
       <tr><td>Three</td><td>Four</td></tr>
       <tr><td>Five</td><td>Six</td></tr>
     </table>
-    <p>Кількість кліків: {{count}}</p>
+    <p>Кількість кліків: {{ count }}</p>
     <button (click)="increase()">Click</button>
   `
 })
@@ -26,15 +26,4 @@ export class AppComponent {
     increase(): void {
         this.count++;
     }
-
-    onNameChange(event: Event): void {
-        const input = event.target as HTMLInputElement;
-        this.name = input.value;
-    }
-
-    onAgeChange(event: Event): void {
-        const input = event.target as HTMLInputElement;
-        this.age = +input.value; // The '+' operator converts the string to a number
-    }
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+
 @NgModule({
-    imports: [ BrowserModule, FormsModule ],
-    declarations: [ AppComponent ],
-    bootstrap: [ AppComponent ]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule // Include FormsModule in the imports array
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
