eed959f57f059f3d12bff1a989277a0a078e5153
Sofi.Maye <sofi.mann99@gmail.com>
2023-11-12 10:41:39 +0200

    lab1 part2 7task css style binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,47 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
-    <div [class.is-red]="isRed" [class.is-blue]="isBlue"></div>
-    <div [style.background-color]="isRed ? 'red' : 'green'"></div>
-    <input type="checkbox" [(ngModel)]="isRed" /> Red
-    <input type="checkbox" [(ngModel)]="isBlue" /> Blue
+    <div [style.background-color]="boxColor" 
+         [style.width.px]="boxSize" 
+         [style.height.px]="boxSize">
+      Dynamic Style Box
+    </div>
+    <div [ngStyle]="boxStyles">
+      Dynamic ngStyle Box
+    </div>
+    <button (click)="changeColor()">Change Color</button>
+    <button (click)="changeSize()">Change Size</button>
   `,
     styles: [`
-    div { width: 50px; height: 50px; border: 1px solid #ccc; margin-bottom: 10px; }
-    .is-red { background-color: red; }
-    .is-blue { background-color: blue; }
-  `]
+        div {
+            display: inline-block;
+            text-align: center;
+            line-height: 50px;
+            margin: 5px;
+            border: 1px solid #ccc;
+        }
+    `]
 })
 export class AppComponent {
-    isRed = false;
-    isBlue = false;
+    boxColor = 'red';
+    boxSize = 150; // size in pixels
+    boxStyles = {
+        'background-color': 'green',
+        'width.px': 200,
+        'height.px': 200,
+        'line-height.px': 100
+    };
+
+    changeColor(): void {
+        this.boxColor = this.boxColor === 'red' ? 'blue' : 'red';
+        this.boxStyles['background-color'] = this.boxStyles['background-color'] === 'green' ? 'purple' : 'green';
+    }
+
+    changeSize(): void {
+        this.boxSize = this.boxSize === 150 ? 300 : 150;
+        const newSize = this.boxStyles['width.px'] === 200 ? 300 : 200;
+        this.boxStyles['width.px'] = newSize;
+        this.boxStyles['height.px'] = newSize;
+        this.boxStyles['line-height.px'] = newSize;
+    }
 }
