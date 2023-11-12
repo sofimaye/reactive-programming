9ac9c3f34e526593ded88e318b825a944cf8f482
Sofi.Maye <sofi.mann99@gmail.com>
2023-11-12 16:13:31 +0200

    tasks 5 for Component1: ng-content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
 @Component({
     selector: 'my-app',
-    template: `
-<label>Введіть ім'я:</label>
+    template: `<label>Введіть ім'я:</label>
+    <input [(ngModel)]="name" placeholder="name">
+    <h1>Ласкаво просимо {{name}}!</h1>
+    <h2>Hello Angular</h2>
+    <p>Angular 16 представляє модульну архітектуру додатку</p>
+    <child-comp><h2>Ласкаво просимо {{name}}!</h2></child-comp>
+    <p>Hello {{name}}</p>
+    `,
+    styles: [` h3 {
+        color: navy;
+    }
 
-<input [(ngModel)]="name" placeholder="name">
-<h1>Ласкаво просимо {{name}}!</h1>
-<h2>Hello Angular</h2>
-<p>Angular 16 представляє модульну архітектуру додатку</p>
-<child-comp></child-comp>
-<p>Hello {{name}}</p>`,
-    styles: [`h2, p {color:#333;}`]
+    h2, p {
+        color: navy;
+    }`]
 })
 export class AppComponent {
     name = 'Петро';
