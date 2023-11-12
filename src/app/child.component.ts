e1aa12f9458ca0c6297ecec124652607a5dd54e1
Sofi.Maye <sofi.mann99@gmail.com>
2023-11-12 18:38:34 +0200

    tasks 10 for Component4: component lifecycle

--- src/app/child.component.ts
+++ src/app/child.component.ts
@@ -1,17 +1,21 @@
-import {Component, Input, Output, EventEmitter} from '@angular/core';
-
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 @Component({
     selector: 'child-comp',
-    template: `
-        <input [ngModel]="userName" (ngModelChange)="onNameChange($event)"/>`
+    template: `<p>Привіт {{name}}</p>`
 })
-
-export class ChildComponent {
-    @Input() userName: string = "";
-    @Output() userNameChange = new EventEmitter<string>();
-
-    onNameChange(model: string) {
-        this.userName = model;
-        this.userNameChange.emit(model);
+export class ChildComponent implements OnInit, OnChanges {
+    @Input() name: string = "";
+    constructor(){ this.log(`constructor`); }
+    ngOnInit() { this.log(`onInit`); }
+    ngOnChanges(changes: SimpleChanges) {
+        for (let propName in changes) {
+            let chng = changes[propName];
+            let cur = JSON.stringify(chng.currentValue);
+            let prev = JSON.stringify(chng.previousValue);
+            this.log(`${propName}: currentValue = ${cur}, previousValue = ${prev}`);
+        }
+    }
+    private log(msg: string) {
+        console.log(msg);
     }
 }
