ead2220cc5131f7d36946165f6df6f47c7b187da
Sofi.Maye <sofi.mann99@gmail.com>
2023-11-12 17:34:34 +0200

    tasks 9 for Component3:  two-way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,11 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
-        <child-comp [userName]="name" [userAge]="age" (onChanged)="onChanged($event)"></child-comp>
-    `,
+        <child-comp [(userName)]="name"></child-comp>
+        <div>Обране ім’я: {{name}}</div>`,
     styleUrls: [`app.component.css`]
 })
 
 export class AppComponent {
     name: string = "Tom";
-    age: number = 24;
-
-    onChanged(increased: any) {
-        increased == true ? this.age++ : this.age--;
-    }
 }
--- src/app/child.component.ts
+++ src/app/child.component.ts
@@ -1,34 +1,17 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
     selector: 'child-comp',
     template: `
-        <p>Ім’я користувача: {{userName}}</p>
-        <p>Вік користувача: {{userAge}}</p>
-        <button (click)="change(true)">+</button>
-        <button (click)="change(false)">-</button>`
+        <input [ngModel]="userName" (ngModelChange)="onNameChange($event)"/>`
 })
 
 export class ChildComponent {
     @Input() userName: string = "";
-    _userAge: number = 0;
-    @Input()
-    set userAge(age: number) {
-        if (age < 0)
-            this._userAge = 0;
-        else if (age > 100)
-            this._userAge = 100;
-        else
-            this._userAge = age;
-    }
+    @Output() userNameChange = new EventEmitter<string>();
 
-    get userAge() {
-        return this._userAge;
-    }
-
-    @Output() onChanged = new EventEmitter<boolean>();
-
-    change(increased: any) {
-        this.onChanged.emit(increased);
+    onNameChange(model: string) {
+        this.userName = model;
+        this.userNameChange.emit(model);
     }
 }
