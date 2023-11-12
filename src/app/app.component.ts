1a2295e852ab32437c9a43eaef07a75ed9b39985
Sofi.Maye <sofi.mann99@gmail.com>
2023-11-12 15:52:47 +0200

    tasks 3 for Component1: extracting the styles and template into separate files and adding them to the component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,26 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `<label>Введіть ім'я:</label>
-    <input [(ngModel)]="name" placeholder="name">
-    <h1>Ласкаво просимо {{name}}!</h1>
-    <h2>Hello Angular</h2>
-    <p>Angular 16 представляє модульну архітектуру додатку</p>`,
-    styles: [`
-        h2, h3 {
-            color: navy;
-        }
-
-        p {
-            font-size: 13px;
-            font-family: Verdana, sans-serif;
-        }
-
-        :host {
-            color: #555;
-        }
-    `]
-
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
     name = '';
