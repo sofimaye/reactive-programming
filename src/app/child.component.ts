d13f7d2b203fcef5ca493612cdcbc2f37682f01e
Sofi.Maye <sofi.mann99@gmail.com>
2023-11-12 16:47:05 +0200

    tasks 6 for Component1: Interaction between components. Passing data to a child component

--- src/app/child.component.ts
+++ src/app/child.component.ts
@@ -1,14 +1,21 @@
-import {Component} from '@angular/core';
+import {Input, Component} from '@angular/core';
 
 @Component({
     selector: 'child-comp',
     template: `
         <ng-content></ng-content>
-        <p>Привіт {{name}}</p>`,
-        styles: [`h2, p {
-        color: red;
-        }`]
+        <p>Привіт {{name}}</p>
+        <p>Ім’я користувача: {{userName}}</p>
+        <p>Вік користувача: {{userAge}}</p>`,
+
+    styles: [`
+        p{color: red}
+        p{font-size:15px}
+    `]
 })
+
 export class ChildComponent {
-    name = "Тарас";
+    name: string = "Тарас";
+    @Input() userName: string = "";
+    @Input() userAge: number = 0;
 }
